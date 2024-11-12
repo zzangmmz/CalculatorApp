1d904cf61fca6e5e5043851a78e421af1f173551
zzangmmz <leemj036@gmail.com>
2024-11-12 17:36:17 +0900

    feat: Level2. 커스텀 버튼 CalculatorButton 정의

---
+++ CalculatorApp/Views/CalculatorButton.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class CalculatorButton: UIButton {
+    init(_ color: UIColor, _ text: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = color
+        self.setTitle(text, for: .normal)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 연산자, 피연산자 버튼 공통 속성
+    func setup() {
+        self.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        self.frame.size.height = 80
+        self.frame.size.width = 80
+        self.layer.cornerRadius = 40
+    }
+}
