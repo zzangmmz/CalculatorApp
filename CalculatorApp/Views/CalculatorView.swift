75e116ae30fe46fabed59c3493c9ef1924d10bbb
zzangmmz <leemj036@gmail.com>
2024-11-12 20:49:30 +0900

    feat: Level3. 모든 버튼 생성, makeVerticalStackView 메서드 추가

--- CalculatorApp/Views/CalculatorView.swift
+++ CalculatorApp/Views/CalculatorView.swift
@@ -11,11 +11,33 @@ final class CalculatorView: UIView {
         return label
     }()
     
-    private let sevenButton = CalculatorButton(.numberColor, "7")
-    private let eightButton = CalculatorButton(.numberColor, "8")
-    private let nineButton = CalculatorButton(.numberColor, "9")
-    private let addButton = CalculatorButton(.numberColor, "+")
+    private lazy var sevenButton = CalculatorButton(.numberColor, "7")
+    private lazy var eightButton = CalculatorButton(.numberColor, "8")
+    private lazy var nineButton = CalculatorButton(.numberColor, "9")
+    private lazy var addButton = CalculatorButton(.numberColor, "+")
     private lazy var sevenToNineStackView = makeHorizontalStackView([sevenButton, eightButton, nineButton, addButton])
+    
+    private lazy var fourButton = CalculatorButton(.numberColor, "4")
+    private lazy var fiveButton = CalculatorButton(.numberColor, "5")
+    private lazy var sixButton = CalculatorButton(.numberColor, "6")
+    private lazy var subtractButton = CalculatorButton(.numberColor, "-")
+    private lazy var fourToSixStackView = makeHorizontalStackView([fourButton, fiveButton, sixButton, subtractButton])
+    
+    private lazy var threeButton = CalculatorButton(.numberColor, "3")
+    private lazy var twoButton = CalculatorButton(.numberColor, "2")
+    private lazy var oneButton = CalculatorButton(.numberColor, "1")
+    private lazy var multipleButton = CalculatorButton(.numberColor, "*")
+    private lazy var oneToThreeStackView = makeHorizontalStackView([threeButton, twoButton, oneButton, multipleButton])
+    
+    private lazy var acButton = CalculatorButton(.numberColor, "AC")
+    private lazy var zeroButton = CalculatorButton(.numberColor, "0")
+    private lazy var equalButton = CalculatorButton(.numberColor, "=")
+    private lazy var divideButton = CalculatorButton(.numberColor, "/")
+    private lazy var zeroStackView = makeHorizontalStackView([acButton, zeroButton, equalButton, divideButton])
+    
+    private lazy var totalStackView: UIStackView = {
+        makeVerticalStackView([sevenToNineStackView, fourToSixStackView, oneToThreeStackView, zeroStackView])
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,13 +51,16 @@ final class CalculatorView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setUI() {
+    private func setUI() {
         self.backgroundColor = .black
     }
     
-    func setConstraints() {
-        printLabel.translatesAutoresizingMaskIntoConstraints = false
-        sevenToNineStackView.translatesAutoresizingMaskIntoConstraints = false
+    private func setSubviews() {
+        [printLabel, totalStackView].forEach { addSubview($0) }
+    }
+    
+    private func setConstraints() {
+        [printLabel, totalStackView].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
         
         NSLayoutConstraint.activate([
             printLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
@@ -44,23 +69,33 @@ final class CalculatorView: UIView {
             printLabel.heightAnchor.constraint(equalToConstant: 100),
             
             sevenToNineStackView.heightAnchor.constraint(equalToConstant: 80),
-            sevenToNineStackView.topAnchor.constraint(equalTo: printLabel.bottomAnchor, constant: 60),
-            sevenToNineStackView.widthAnchor.constraint(equalToConstant: 350),
-            sevenToNineStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            fourToSixStackView.heightAnchor.constraint(equalToConstant: 80),
+            oneToThreeStackView.heightAnchor.constraint(equalToConstant: 80),
+            zeroStackView.heightAnchor.constraint(equalToConstant: 80),
+            
+            totalStackView.widthAnchor.constraint(equalToConstant: 350),
+            totalStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            totalStackView.topAnchor.constraint(equalTo: printLabel.bottomAnchor, constant: 60)
         ])
     }
     
-    func setSubviews() {
-        [printLabel, sevenToNineStackView].forEach { addSubview($0) }
-    }
-    
-    func makeHorizontalStackView(_ views: [UIView]) -> UIStackView {
+    private func makeHorizontalStackView(_ views: [UIView]) -> UIStackView {
         let stview = UIStackView()
         stview.axis = .horizontal
         stview.backgroundColor = .black
         stview.spacing = 10
         stview.distribution = .fillEqually
         views.forEach { stview.addArrangedSubview($0) }
+        return stview
+    }
+    
+    private func makeVerticalStackView(_ stackViews: [UIStackView]) -> UIStackView {
+        let stview = UIStackView()
+        stview.axis = .vertical
+        stview.backgroundColor = .black
+        stview.spacing = 10
+        stview.distribution = .fillEqually
+        stackViews.forEach { stview.addArrangedSubview($0) }
         return stview
     }
 }
